5eb1c113631bcb448120b38ae4ef3eaa8e06b156
RaTe1967 <115639425+RaTe1967@users.noreply.github.com>
2022-10-12 18:45:33 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,62 @@
 input.onButtonPressed(Button.A, function () {
+    řádek()
+})
+input.onButtonPressed(Button.B, function () {
+    prodleva += ZMĚNA
+    music.playTone(prodleva, music.beat(BeatFraction.Double))
+    if (prodleva == 500 || prodleva == 100) {
+        ZMĚNA = ZMĚNA * -1
+    }
+    if (prodleva == 500) {
+        led.plot(4, 4)
+    } else {
+        led.unplot(4, 4)
+        if (prodleva > 300) {
+            led.plot(3, 4)
+        } else {
+            led.unplot(3, 4)
+            if (prodleva > 200) {
+                led.plot(2, 4)
+            } else {
+                led.unplot(2, 4)
+                if (prodleva > 100) {
+                    led.plot(1, 4)
+                } else {
+                    led.unplot(1, 4)
+                }
+            }
+        }
+    }
+})
+function řádek () {
     for (let pořadí = 0; pořadí <= 4; pořadí++) {
         led.unplot(pořadí, 0)
     }
     basic.pause(100)
     for (let pořadí = 0; pořadí <= 4; pořadí++) {
-        led.plot(pořadí, 0)
+        led.plotBrightness(pořadí, 0, jas)
         basic.pause(prodleva)
     }
-})
-input.onButtonPressed(Button.B, function () {
-    prodleva += ZMĚNA
-    if (prodleva == 500 || prodleva == 100) {
-        ZMĚNA = ZMĚNA * -1
+}
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    jas += ZMENA_JAS
+    if (jas == 250 || jas == 50) {
+        ZMĚNA = ZMENA_JAS * -1
     }
+    control.raiseEvent(
+    EventBusSource.MICROBIT_ID_BUTTON_A,
+    EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+    )
 })
+let ZMENA_JAS = 0
+let jas = 0
 let ZMĚNA = 0
 let prodleva = 0
 basic.showIcon(IconNames.StickFigure)
 prodleva = 100
 ZMĚNA = 100
+jas = 50
+ZMENA_JAS = 50
 basic.pause(2000)
 basic.clearScreen()
 led.plot(0, 4)
